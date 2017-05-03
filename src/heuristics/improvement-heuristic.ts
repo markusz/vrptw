48720ba10ac4415dd448e29e5eb597e6e60a3ba9
Markus Ziller <mail.markus.ziller@gmail.com>
2017-05-03 18:07:36 +0200

    Initial commit

---
+++ src/heuristics/improvement-heuristic.ts
@@ -0,0 +1,7 @@
+///<reference path="../models/solver/solution.ts"/>
+
+export default class ImprovmentHeuristic {
+  static improveSolution(s: Solution) {
+    return s
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log('123');
+
+import ImprovementHeuristic from "./heuristics/improvement-heuristic"
+
+ImprovementHeuristic.improveSolution('ASDASd');
