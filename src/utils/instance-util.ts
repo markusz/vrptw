dc4f73ebce4512bcc334faaf1960e4a60b07e747
Markus Ziller <mail.markus.ziller@gmail.com>
2017-05-05 18:39:02 +0200

    Adds utils

---
+++ src/utils/instance-util.ts
@@ -0,0 +1,46 @@
+import Problem from "../models/solver/problem";
+import Customer from "../models/customer";
+import Node from "../models/node";
+import * as lodash from 'lodash';
+
+export default class InstanceUtil {
+    static importProblem(problemClass: string, problemId: string): Problem {
+        const rawInstanceData = require(`../../data/instances/${problemClass}/${problemId}.json`);
+        const customers: Array<Customer> = InstanceUtil.getCustomers(rawInstanceData);
+        const depot: Node = InstanceUtil.getDepot(rawInstanceData);
+        const distances: Array<Array<number>> = rawInstanceData.distance_matrix;
+        const constraints: any = InstanceUtil.getConstraints(rawInstanceData);
+        const meta: any = {
+            problemClass: problemClass,
+            problemId: problemId
+        };
+
+        return new Problem(customers, depot, distances, constraints, meta);
+    }
+
+    private static getDepot(data: any) {
+        const depotData = data.deport;
+        depotData.id = 0;
+        return new Node(depotData);
+    }
+
+    private static getConstraints(data: any) {
+        return {
+            maxVehicles: data.max_vehicle_number,
+            vehicleCapacity: data.vehicle_capacity,
+            start: data.deport.ready_time,
+            end:  data.deport.due_time
+        }
+    }
+
+    private static getCustomers(data: any) {
+        return Object.keys(data)
+            .filter(key => lodash.startsWith(key, 'customer_'))
+            .map(key => {
+                data[key].id = parseInt(key.split('_')[1]);
+                return data[key];
+            })
+            .sort((a, b) => a.id - b.id)
+            .map(customerData => new Customer(customerData));
+    }
+}
--- src/utils/time-util.ts
+++ src/utils/time-util.ts
@@ -1,3 +1,23 @@
-/**
- * Created by zil0002m on 03.05.17.
- */
+import TimeWindow from "../models/time-window";
+export default class TimeUtil {
+    static doesTimeWindowIncludeTimeWindow(includer: TimeWindow, includee: TimeWindow) {
+        const lowerBoundOk = includer.start <= includee.start;
+        const upperBoundOk = includer.end >= includer.end;
+
+        return lowerBoundOk && upperBoundOk
+    }
+
+    static doTimeWindowsOverlap(first: TimeWindow, second: TimeWindow): boolean {
+        return !(first.start > second.end || second.start > first.end)
+    }
+
+    static getOverlap(first: TimeWindow, second: TimeWindow): TimeWindow {
+        if(!TimeUtil.doTimeWindowsOverlap(first, second)) {
+            throw new Error('No Overlap')
+        }
+
+        const start = Math.max(first.start, second.start);
+        const end = Math.min(first.end, second.end);
+        return new TimeWindow(start, end);
+    }
+}
